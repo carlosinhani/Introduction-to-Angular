7e16b34beaf2c201e03d50984f9f28cd904d75b0
Carlos.Inhani <carlos.inhani@gmail.com>
2020-11-19 20:59:22 -0300

    ng_module

--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -16,16 +16,26 @@ export class CourseListComponent implements OnInit {
    constructor(private courseService: CourseService) { }
 
   ngOnInit(): void {
-      this.retriveAll();
+      this.retrieveAll();
   }
 
-  retriveAll(): void {
+  retrieveAll(): void {
     this.courseService.retrieveAll().subscribe({
       next: courses => {
          this._courses = courses;
          this.filteredCourses = this._courses;
         },
         error: err => console.log("Error", err)
+    })
+  }
+
+  deleteById(courseId: number): void {
+    this.courseService.deleteById(courseId).subscribe({
+      next: () => {
+        console.log('Deleted with success');
+        this.retrieveAll();
+      },
+      error: err => console.log('Error', err)
     })
   }
 
--- src/app/courses/course.service.ts
+++ src/app/courses/course.service.ts
@@ -29,6 +29,10 @@ export class CourseService {
            return this.httpClient.post<Course>(`${this.coursesUrl}`, course);
        }
      }
+
+     deleteById(id: number): Observable<any> {
+        return this.httpClient.delete<any>(`${this.coursesUrl}/${id}`);
+     }
 }
 
 var COURSES: Course[] = [
