f572d877276eb6977a4c605b1ddf916ebcab2037
Carlos.Inhani <carlos.inhani@gmail.com>
2020-11-19 12:20:46 -0300

    router_component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,8 @@ import { CourseListComponent } from './courses/course-list.component';
 import { StarComponent } from './star/star.component';
 import { ReplacePipe } from './pipe/replace.pipe';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
-import { importExpr } from '@angular/compiler/src/output/output_ast';
-import { ImplicitReceiver } from '@angular/compiler';
 import { Error404Component } from './error-404/error-404.component';
+import { CourseInfoComponent } from './courses/course-info.component';
 
 @NgModule({
   declarations: [
@@ -20,16 +19,20 @@ import { Error404Component } from './error-404/error-404.component';
     ReplacePipe,
     NavBarComponent,
     Error404Component,
+    CourseInfoComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     RouterModule.forRoot([
       {
-        path: '', redirectTo: 'courses', pathMatch: 'full'
+        path: 'courses', component: CourseListComponent
       },
       {
-        path: 'courses', component: CourseListComponent
+        path: 'courses/info/:id', component: CourseInfoComponent
+      },
+      {
+        path: '', redirectTo: 'courses', pathMatch: 'full'
       },
       {
         path:'**', component: Error404Component
