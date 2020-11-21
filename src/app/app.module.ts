2bd9c167cd9bb4ca819623fa24b86a520dc21caf
Carlos.Inhani <carlos.inhani@gmail.com>
2020-11-21 08:10:37 -0300

    Finished_ANGULAR_Introduction

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,12 @@ import { RouterModule } from "@angular/router";
 import { HttpClientModule } from "@angular/common/http";
 
 import { AppComponent } from './app.component';
-import { Error404Component } from './error-404/error-404.component';
 import { CourseModule } from './courses/courses.module';
 import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    Error404Component
   ],
   imports: [
     BrowserModule,
@@ -21,12 +19,9 @@ import { CoreModule } from './core/core.module';
     RouterModule.forRoot([
       {
         path: '', redirectTo: 'courses', pathMatch: 'full'
-      },
-      {
-        path:'**', component: Error404Component
       }
-    ])
 
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,13 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NavBarComponent } from './component/nav-bar/nav-bar.component';
+import { Error404Component } from "./component/error-404/error-404.component";
 
 @NgModule({
   declarations:[
-    NavBarComponent
+    NavBarComponent,
+    Error404Component
   ],
   imports:[
-     RouterModule
+     RouterModule.forChild([
+      {
+        path:'**', component: Error404Component
+      }
+     ])
   ],
   exports:[
     NavBarComponent
